e80f807e8edc0a602050432bf1d36ec4d0feec06
JAHG07 <harogjanto@gmail.com>
2023-09-22 19:59:57 -0500

    Insertion Sort

--- Algorithms/Java/Union_Find/Sort/InsertionSort.java
+++ Algorithms/Java/Union_Find/Sort/InsertionSort.java
@@ -1,21 +1,21 @@
 package Sort;
 
 public class InsertionSort {
-    public static void main(String[] args){
+    public static  void  main(String[] args){
         int[] sort = {2,9,7,6,4,3,1,5,8};
-        for (int i = 0 ; i < 9; i++){
-            int min = i;
-            for (int j = i+1 ; j < 9 ; j ++) {
-                if (sort[j] < sort[min]) min = j;
-            }
-            if (sort[min] < sort[i]){
-              int temp = sort[i];
-              sort[i] = sort[min];
-              sort[min] = temp;
+        for (int i = 1 ; i < 9 ; i++){
+            int j = i;
+            if (sort[i] < sort[i-1]){
+                while (j> 0 && sort[j]<sort[j-1]) {
+                    int temp = sort[j];
+                    sort[j] = sort[j-1];
+                    sort[j-1] = temp;
+                    j--;
+                }
             }
         }
-        for (int k = 0 ; k < 9; k++){
-            System.out.println(sort[k]);
+        for (int x: sort) {
+            System.out.println(x);
         }
     }
 }
