7dca8f9289d5fcbfbb7056d868b9b378dec722f5
JAHG07 <harogjanto@gmail.com>
2023-09-23 20:33:09 -0500

    React Ig

--- React/tic-tac-toe/src/InstaCard.jsx
+++ React/tic-tac-toe/src/InstaCard.jsx
@@ -2,14 +2,14 @@ import './InstaCard.css'
 
 export const InstaCard = ({ username, ago, update }) => {
 
-  const userImage = `https://unavatar.io/${username}`
-
+  const USER_IMG = `https://unavatar.io/${username}`
+  const PUBLICATION_IMG = `https://c4.wallpaperflare.com/wallpaper/946/632/921/espacio-estrellas-naturaleza-planetas-wallpaper-preview.jpg`
   return (
     <article className="ig-card">
       <header className="ig-card-header">
         <div className="ig-card-info">
           <div className="ig-card-info__img">
-            <img src={userImage} alt="User image" />
+            <img src={USER_IMG} alt="User image" />
           </div>
           <div className="ig-card-info__user">
             <div>
@@ -24,7 +24,14 @@ export const InstaCard = ({ username, ago, update }) => {
       </header>
       <main className="">
         <div className="publication-img">
-          <img src="" alt="" />
+        </div>
+        <div className="publication-footer">
+          <div className="left">
+            like
+          </div>
+          <div className="rigth">
+            guardar
+          </div>
         </div>
       </main>
       <footer>
