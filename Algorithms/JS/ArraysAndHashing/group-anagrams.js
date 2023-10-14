d768e7ab409a4dec6cfd953aa9c2658938a3d18d
JAHG07 <harogjanto@gmail.com>
2023-10-14 17:48:41 -0500

    Group-anagram

--- Algorithms/JS/ArraysAndHashing/group-anagrams.js
+++ Algorithms/JS/ArraysAndHashing/group-anagrams.js
@@ -2,10 +2,33 @@ let strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
 var groupAnagrams = function (strs) {
   const groupAn = new Map()
-  strs.forEach(str => {
-    let temp = str.split('').sort((a, b) => a - b)
-    console.log(temp)
-  });
+  for (let i = 0; i <= strs.length - 1; i++) {
+    let temp = strs[i].split('').sort().join('')
+    if (groupAn.has(temp)) {
+      groupAn.get(temp).push(strs[i])
+    } else {
+      groupAn.set(temp, [])
+      groupAn.get(temp).push(strs[i])
+    }
+  }
+  return [...groupAn.values()]
 };
 
-console.log(groupAnagrams(strs))
+var groupAnagrams1 = function (strs, groupAn = new Map()) {
+  for (const s of strs) {
+    const count = new Array(26).fill(0)
+    for (const c of s) {
+      count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+    }
+    const newCount = count.toString()
+    if (groupAn.has(newCount)) {
+      groupAn.get(newCount).push(s)
+    } else {
+      groupAn.set(newCount, [])
+      groupAn.get(newCount).push(s)
+    }
+  }
+  return [...groupAn.values()]
+};
+
+console.log(groupAnagrams1(strs))
