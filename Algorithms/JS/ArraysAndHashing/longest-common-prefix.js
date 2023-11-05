6f9d9743156c28d1ae2db8e3b6a43b1576a743af
JAHG07 <harogjanto@gmail.com>
2023-11-05 14:46:53 -0600

    longest common prefix

--- Algorithms/JS/ArraysAndHashing/longest-common-prefix.js
+++ Algorithms/JS/ArraysAndHashing/longest-common-prefix.js
@@ -1,11 +1,16 @@
-let strs = ["flower", "flow", "flight"]
+let strs = ["carton", "cart", 'c']
 
 var longestCommonPrefix = function (strs) {
-  let res = []
-  if (strs.length == 1) return strs[0]
-  let maxPrefix = strs[0].split('')
+  if (!strs.length) return '';
 
-  return maxPrefix
+  for (let i = 0; i <= strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+  return strs[0];
 };
 
 console.log(longestCommonPrefix(strs))
