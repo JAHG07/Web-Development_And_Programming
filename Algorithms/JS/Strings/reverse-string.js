a9c5547b33c9fbe748622b6c94fa8fa0e2fb73b1
JAHG07 <harogjanto@gmail.com>
2023-11-06 14:28:00 -0600

    reverse a string

---
+++ Algorithms/JS/Strings/reverse-string.js
@@ -0,0 +1,17 @@
+let s = ["h","e","l","l","o"]
+
+var reverseString = function(s) {
+  // [s[l],s[r]] = [s[r],r[l]]
+  for(let l = 0, r = s.length-1; l < r; l++,r--){
+    swap(s,l,r)
+  }
+  return s
+};
+
+function swap(s,l,r){
+  let temp = s[l]
+  s[l] = s[r]
+  s[r] = temp
+}
+
+console.log(reverseString(s))
