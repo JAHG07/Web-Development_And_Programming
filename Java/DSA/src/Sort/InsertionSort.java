4897fd4fa4a550ed8314819eda940bac1d0cce09
JAHG07 <harogjanto@gmail.com>
2024-01-12 17:43:18 -0600

    insertion sort

--- Java/DSA/src/Sort/InsertionSort.java
+++ Java/DSA/src/Sort/InsertionSort.java
@@ -1,4 +1,25 @@
 package Sort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {9,8,7,6,5,4,3,2,1};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if(arr[j]>arr[j-1]) break;
+                swap(arr,j,j-1);
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
